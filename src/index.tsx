1eefcbcfd909525e0a942c614a88c6c568bb2acd
Steve Hartken <steve.hartken@gmail.com>
2020-05-09 17:40:14 -0400

    Diez import, typescript react scaffolding, push for Readme

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,17 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import { DesignLanguage, Diez } from '../diez/thisbounty-styles/design-language/build/diez-thisbounty-styles-web/index'
+
 
 import { Hello } from "./components/Hello";
 
-ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
-    document.getElementById("example")
-);
+// Create a new Diez instance providing your DesignLanguage as a source
+const diezDs = new Diez(DesignLanguage);
+// Listen to changes in the design language
+diezDs.attach((ds) => {
+    // The ds has been updated!
+    ReactDOM.render(
+        <Hello compiler="TypeScript" framework="React" />,
+        document.getElementById("example")
+    );
+});
