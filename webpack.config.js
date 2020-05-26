cdceec5ab5b2480c4c3c1ccf59eb61f462e77718
Steve Hartken <steve.hartken@gmail.com>
2020-05-25 20:42:23 -0400

    Netlify -- ds in webpack externals

--- webpack.config.js
+++ webpack.config.js
@@ -87,6 +87,7 @@ module.exports = {
   // dependencies, which allows browsers to cache those libraries between builds.
   externals: {
     react: 'React',
-    'react-dom': 'ReactDOM'
+    'react-dom': 'ReactDOM',
+    '@thisbounty/styles': 'thisbounty/styles'
   }
 }
