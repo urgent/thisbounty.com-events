626648193eb8f26ac720a386340691cb4cc61b3d
Steve Hartken <steve.hartken@gmail.com>
2020-07-13 15:59:04 -0400

    #24 -- create
    More tests for create, attempting to standardize effect testing approach

--- src/components/Lead/create.effect.test.js
+++ src/components/Lead/create.effect.test.js
@@ -9,6 +9,43 @@ const deps = { setLeads: () => { }, bounty: "1" }
 const data = JSON.stringify(valid);
 const event = { data }
 
+test('make returns a function for valid state and message', () => {
+  const unit = make([])(event);
+  expect(unit).toEqual(expect.any(Function))
+});
+
+test('create mutates states for specified bounty by id', () => {
+  let state = { bounty: "1", leads: { "1": [{ suit: "H", number: "K" }, { suit: "H", number: "A" }], "2": [{ suit: "D", number: "K" }, { suit: "D", number: "A" }] } }
+  const setLeads = (update) => state.leads = update;
+  // first parameter is empty. state.leads["1"] overwritten. Used here, not in production, for easier equality test
+  create([])({ bounty: "1", leads: state, setLeads, setBounty: () => { } })(event);
+  expect(state.leads["1"]).toEqual([{ suit: "H", number: 2 }]);
+});
+
+test('create isolates state mutation by bounty', () => {
+  let state = { bounty: "1", leads: { "1": [{ suit: "H", number: "K" }, { suit: "H", number: "A" }], "2": [{ suit: "D", number: "K" }, { suit: "D", number: "A" }] } }
+  const setLeads = (update) => state.leads = update
+  create([])({ bounty: "1", leads: state.leads, setLeads, setBounty: () => { } })(event);
+  expect(state.leads["2"]).toEqual([{ suit: "D", number: "K" }, { suit: "D", number: "A" }]);
+});
+
+test('create returns a function for valid event', () => {
+  const unit = create([])(deps)(event);
+  expect(unit).toEqual(expect.any(Function))
+});
+
+test('create returns an error for invalid event', () => {
+  const number = create([])(deps)({ data: JSON.stringify(misnumber) });
+  expect(number).toEqual(expect.any(Error))
+  const suit = create([])(deps)({ data: JSON.stringify(missuit) });
+  expect(suit).toEqual(expect.any(Error))
+});
+
+test('create returns an error for duplicate event', () => {
+  const unit = create([valid])(deps)(event);
+  expect(unit).toEqual(expect.any(Error))
+});
+
 test('Merge right', () => {
   const unit = merge([])(valid);
   const result = right([valid]);
@@ -45,36 +82,4 @@ test('onRuntime returns a Task', () => {
 test('onMerge returns a Task', () => {
   const prompt = onMerge([valid])
   expect(prompt).toEqual(expect.any(Function))
-});
-
-test('make returns a function', () => {
-  const unit = make([])(event)(deps);
-  expect(unit).toEqual(expect.any(Function))
-});
-
-test('create mutates states for specified bounty by id', () => {
-  let state = { bounty: "1", leads: { "1": [{ suit: "H", number: "K" }, { suit: "H", number: "A" }], "2": [{ suit: "D", number: "K" }, { suit: "D", number: "A" }] } }
-  const setLeads = (update) => state.leads = update;
-  // first parameter is empty. state.leads["1"] overwritten. Used here, not in production, for easier equality test
-  create([])({ bounty: "1", leads: state, setLeads, setBounty: () => { } })(event);
-  expect(state.leads["1"]).toEqual([{ suit: "H", number: 2 }]);
-});
-
-test('create isolates state mutation by bounty', () => {
-  let state = { bounty: "1", leads: { "1": [{ suit: "H", number: "K" }, { suit: "H", number: "A" }], "2": [{ suit: "D", number: "K" }, { suit: "D", number: "A" }] } }
-  const setLeads = (update) => state.leads = update
-  create([])({ bounty: "1", leads: state.leads, setLeads, setBounty: () => { } })(event);
-  expect(state.leads["2"]).toEqual([{ suit: "D", number: "K" }, { suit: "D", number: "A" }]);
-});
-
-test('create returns a function for valid event', () => {
-  const unit = create([])(deps)(event);
-  expect(unit).toEqual(expect.any(Function))
-});
-
-test('create returns an error for invalid event', () => {
-  const number = create([])(deps)({ data: JSON.stringify(misnumber) });
-  expect(number).toEqual(expect.any(Error))
-  const suit = create([])(deps)({ data: JSON.stringify(missuit) });
-  expect(suit).toEqual(expect.any(Error))
 });
