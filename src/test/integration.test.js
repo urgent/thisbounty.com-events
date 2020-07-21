a625cdac1982fef7c5d861cb7f34be3d4c43662e
Steve Hartken <steve.hartken@gmail.com>
2020-07-21 16:36:54 -0400

    #31 -- Integration tests
    Need timeout in async function:
    https://www.pentarem.com/blog/how-to-use-settimeout-with-async-await-in-javascript/
    
    And xpaths:
    https://stackoverflow.com/questions/23676537/xpath-for-button-having-text-as-new#23677077

--- src/test/integration.test.js
+++ src/test/integration.test.js
@@ -3,12 +3,39 @@ const socket = new WebSocket(
     'wss://connect.websocket.in/v3/1?apiKey=66FzbgigXbiN77D7eYNEQBJ0F0SGGXfhGonNoNYz7IejUldW82tOUn7kT5gO'
 )
 
-describe('Google', () => {
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+describe('Leads', () => {
     beforeAll(async () => {
-        await page.goto('https://google.com');
+        await page.goto('http://localhost:8080');
     });
 
-    it('should be titled "Google"', async () => {
-        await expect(page.title()).resolves.toMatch('Google');
+    it('should accept leads"', async () => {
+        jest.setTimeout(5000);
+        // no lead exists
+        const prior = await page.$x("//button[text()='J']");
+        expect(prior.length).toEqual(0);
+        // send websocket payload to generate lead
+        setTimeout(() => {
+            socket.send(
+                JSON.stringify({
+                    event: 'RESPONSE_LEADS',
+                    data: {
+                        '1': [
+                            { suit: 'H', number: 'J' },
+                            { suit: 'H', number: 'Q' },
+                            { suit: 'H', number: 'K' },
+                            { suit: 'H', number: 'A' }
+                        ]
+                    }
+                })
+            )
+        }, 1000)
+        await delay(2500);
+        // lead now exists
+        const update = await page.$x("//button[text()='J']");
+        expect(update.length).toEqual(1);
     });
 });
